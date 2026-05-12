0de9bbfa9f529f3b4a5fa2ca36e99baba22b78c7
sachinstha9 <270480510@yoobeestudent.ac.nz>
2026-05-12 14:37:08 +1200

    this much for may 12

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def product_formatter(products):
             "id": product[0],
             "title": product[1],
             "description": product[2],
-            "img_sources": product[3].replace("[", "").replace("]", "").split(","),
+            "img_sources": json.loads(product[3]),
             "tags": product[4].split(","),
             "price": product[5],
             "specifications": json.loads(product[6]),
@@ -28,15 +28,19 @@ def product_formatter(products):
 def index():
     conn = sqlite3.connect("computer-ecommerce.db")
     c = conn.cursor()
-    c.execute('SELECT * FROM products WHERE in_banner = ?', (1,))
 
+    c.execute('SELECT * FROM products')
+    all_products = c.fetchall()
+
+    c.execute('SELECT * FROM products WHERE in_banner = ?', (1,))
     in_banner_products = c.fetchall()
 
     conn.close()
 
+    all_products = product_formatter(all_products)
     in_banner_products = product_formatter(in_banner_products)
 
-    return render_template("index.html", in_banner_products=in_banner_products)
+    return render_template("index.html", in_banner_products=in_banner_products, all_products=all_products)
 
 if __name__ == "__main__":
     app.run(debug=True)
